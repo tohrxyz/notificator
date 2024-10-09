5a5549e503d41e58d08e13318833c4004407a8c7
tohrxyz <tomas.hrib@proton.me>
2024-10-09 18:58:20 +0200

    periodically run

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"notificator/main/lib"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -27,32 +28,37 @@ func main() {
 	if storePath == "" {
 		fmt.Println("Matrix store directory path not found in your environment vars.")
 	}
-	position, err := lib.QueryPosition()
-	if err != nil {
-		fmt.Printf("err.Error(): %v\n", err.Error())
+
+	for true {
+		position, err := lib.QueryPosition()
+		if err != nil {
+			fmt.Printf("err.Error(): %v\n", err.Error())
+		}
+
+		fmt.Print(position + "\n")
+
+		totalDeposit, totalBorrow, err := lib.ProcessSum(position)
+		if err != nil {
+			fmt.Printf("err.Error(): %v\n", err.Error())
+		}
+
+		fmt.Printf(`Total deposits: %v\n`, totalDeposit)
+		fmt.Printf(`Total borrows: %v\n`, totalBorrow)
+
+		healthFactor := math.Round(float64((totalDeposit*0.78)/totalBorrow)*100) / 100
+
+		println("Health factor: ", healthFactor)
+
+		msg := fmt.Sprintf(`Health Factor: %v`, healthFactor)
+
+		cmd := exec.Command(matrixCommanderPath, "-m", msg, "-c", credentialsFilePath, "-s", storePath)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Error with sending msg: %s\n", err)
+		}
+
+		fmt.Println(string(output))
+
+		time.Sleep(15 * time.Second)
 	}
-
-	fmt.Print(position + "\n")
-
-	totalDeposit, totalBorrow, err := lib.ProcessSum(position)
-	if err != nil {
-		fmt.Printf("err.Error(): %v\n", err.Error())
-	}
-
-	fmt.Printf(`Total deposits: %v\n`, totalDeposit)
-	fmt.Printf(`Total borrows: %v\n`, totalBorrow)
-
-	healthFactor := math.Round(float64((totalDeposit*0.78)/totalBorrow)*100) / 100
-
-	println("Health factor: ", healthFactor)
-
-	msg := fmt.Sprintf(`Health Factor: %v`, healthFactor)
-
-	cmd := exec.Command(matrixCommanderPath, "-m", msg, "-c", credentialsFilePath, "-s", storePath)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Error with sending msg: %s\n", err)
-	}
-
-	fmt.Println(string(output))
 }
